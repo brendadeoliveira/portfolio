5dd10fa68494c2baf023375acaf91de7417f6dd2
Brenda <75865224+brendadeoliveira@users.noreply.github.com>
2022-05-09 23:33:16 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const EnglishToPt = document.getElementById('eng-pt');
+
+EnglishToPt.addEventListener('mouseover', () => {
+    EnglishToPt.innerHTML = "Oi gente!"
+});
+
+EnglishToPt.addEventListener('mouseout', () => {
+    EnglishToPt.innerHTML = "Hi there!"
+});
